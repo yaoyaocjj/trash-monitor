5582d2b6073bc18b6929969c8764a8d0dc61a628
juanjuan.chen <juanjuan.chen@rea-group.com>
2018-11-15 09:21:07 +0800

    Trash monitor page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './lib/westeros';
 import './App.css';
+import getData from './httpRequest'
+import drawLine from './chart/line'
 
+const mockData = [
+  {
+    name: "percentage",
+    value: [10, 20, 30, 40, 50]
+  },
+  {
+    name: "count",
+    value: [3, 5, 15, 18, 20]
+  }
+]
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      percentage: 0,
+      count: 0
+    }
+  }
+  componentWillMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    getData().then(response => {
+      this.setState({
+        percentage: response.percentage,
+        count: response.count
+      })
+    })
+  }
+
+  componentDidMount() {
+    drawLine(document.getElementById('graph-line'), "trash-monitor-result", mockData)
+    // setInterval(() => this.fetchData, 10000)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="title">Trash Monitor</h1>
         </header>
+        <section className="trash-info">
+          <div className="trash-image">
+            <img src="https://img.icons8.com/office/300/000000/full-trash.png" />
+          </div>
+          <div className="trash-description">
+            <h2>今天你扔垃圾了吗</h2>
+            <p>
+              垃圾入箱、举手之劳。
+              垃圾混置是垃圾，垃圾分类是资源。
+              举手之劳，资源永续的源泉。
+              公德竞赛，今天正式开赛。
+              让环保扎根现在，用绿色昭示未来。
+            </p>
+          </div>
+        </section>
+        <section className="trash-graph">
+          <div className="trash-description">
+            <h2>今天你扔垃圾了吗</h2>
+            <p>
+              垃圾入箱、举手之劳。
+              垃圾混置是垃圾，垃圾分类是资源。
+              举手之劳，资源永续的源泉。
+              公德竞赛，今天正式开赛。
+              让环保扎根现在，用绿色昭示未来。
+            </p>
+          </div>
+          <div id="graph-line" />
+        </section>
       </div>
     );
   }
---
+++ src/chart/line.js
@@ -0,0 +1,42 @@
+import echarts from 'echarts'
+
+export default function drawLine(dom, title, data) {
+  const legend = data.map(function (ele) {
+    return ele.name;
+  });
+  const seriesData = data.map(function (ele) {
+    return {
+      name: ele.name,
+      type: 'line',
+      data: ele.value
+    };
+  });
+
+  const charts = echarts.init(dom, 'westeros');
+
+  charts.setOption({
+    title: {
+      text: title,
+      subtext: '',
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: legend,
+      bottom: '1%'
+    },
+    xAxis: {
+      type: 'category',
+      data: ['first hour', 'second hour', 'third hour', 'forth hour', 'fifth hour']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: seriesData
+  });
+}
